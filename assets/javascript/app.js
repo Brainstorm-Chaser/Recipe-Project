fd47e4298ce2ea2066e4a0345dd12df937a7c8b3
Minu James <minujames@minus-mbp.gateway.pace.com>
2017-10-07 19:29:47 -0700

    adding the code for displying recipes serach result

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,74 @@
 $(document).ready(function(){
-
+      
   $("#search").on("click", function(){
-    var recipeName = $("#search-input").val();
+    var recipeName = $("#search-input").val().trim();
 
+    // $.ajax({
+    //   type: "GET",
+    //   url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/site/search",
+    //   dataType: "json",
+    //   data: jQuery.param({query: recipeName}),
+    //   headers: {
+    //     'X-Mashape-Key': 'LvriMxRsKAmshM4K2IHnxi8ZrwOUp1mrqSCjsnsOdiLEfjwK75'
+    //   }
+    // }).done(function(response) {
+    //   // console.log("response", JSON.stringify(response));
+    //   console.log("response", response);
+    //   displayRecipes(response);
+    // });
+
+    displayRecipes(searchResponse);
 
   });
+
+  function displayRecipes(searchResponse){
+    var recipes = searchResponse.Recipes;
+
+    recipes.forEach(function(recipe){
+      var name = recipe.name;
+      var image = recipe.image;
+      var link = recipe.link;
+      var dataPoints = recipe.dataPoints;
+      var id = getRecipeId(link);
+      console.log("id", id);
+      
+      var recipeDiv = $("<div>").addClass("panel panel-default");
+      var recipeHeader = $("<div>").addClass("panel-heading");
+      var recipeBody = $("<div>").addClass("panel-body");
+
+      var recipeName = $("<h3>").addClass("panel-title").text(name);
+      recipeHeader.html(recipeName);
+
+      var recipeImg = $("<img>").attr("src", image).addClass("recipe-image");
+      var recipeImgLink = $("<a>").attr("href", link).attr('target','_blank').append(recipeImg);
+      
+      var recipeInfoTable = getRecipeInfoTable(dataPoints);
+      recipeInfoTable.addClass("display-recipe");
+
+      recipeBody.append(recipeImgLink);
+      recipeBody.append(recipeInfoTable);
+
+      recipeDiv.append(recipeHeader);      
+      recipeDiv.append(recipeBody);
+
+      $("#recipes-wrapper").append(recipeDiv);
+    });
+  }
+
+  function getRecipeId(link){
+    var idStartIndex = link.lastIndexOf("-") + 1;
+    return link.substring(idStartIndex, link.length);
+  }
+
+  function getRecipeInfoTable(dataPoints){
+    var table = $("<table>");
+    dataPoints.forEach(function(dataPoint){
+      var row = $("<tr>");
+      row.append($("<td>").text(dataPoint.key));
+      row.append($("<td>").text(dataPoint.value));
+      table.append(row);
+    });
+    return table;
+  }
 
 });
---
+++ assets/javascript/response.js
@@ -0,0 +1,45 @@
+var searchResponse = {
+  Recipes:
+  [
+    {
+      name:"Spaghettiless Spaghetti",
+      image:"https://webknox.com/recipeImages/104979-556x370.jpg",
+      link:"https://spoonacular.com/recipes/spaghettiless-spaghetti-104979",
+      kvtable:"<table><tr><td>Cost</td><td>$2.05 per serving</td></tr><tr><td>Calories</td><td>211 Calories</td></tr><tr><td>Protein</td><td>19g Protein</td></tr><tr><td>Fat</td><td>5g Total Fat</td></tr><tr><td>Carbs</td><td>22g Carbs</td></tr></table>",
+      dataPoints:[
+        {key:"Cost",value:"$2.05 per serving",show:true,single:true,sort:null},
+        {key:"Calories",value:"211 Calories",show:true,single:true,sort:null},
+        {key:"Protein",value:"19g Protein",show:true,single:true,sort:null},
+        {key:"Fat",value:"5g Total Fat",show:true,single:true,sort:null},
+        {key:"Carbs",value:"22g Carbs",show:true,single:true,sort:null}
+      ]
+    },
+    {
+      name:"Spaghetti Squash Spaghetti",
+      image:"https://webknox.com/recipeImages/559452-556x370.jpg",
+      link:"https://spoonacular.com/recipes/spaghetti-squash-spaghetti-559452",
+      kvtable:"<table><tr><td>Cost</td><td>$2.05 per serving</td></tr><tr><td>Calories</td><td>301 Calories</td></tr><tr><td>Protein</td><td>16g Protein</td></tr><tr><td>Fat</td><td>13g Total Fat</td></tr><tr><td>Carbs</td><td>34g Carbs</td></tr></table>",
+      dataPoints:[
+        {key:"Cost",value:"$2.05 per serving",show:true,single:true,sort:null},
+        {key:"Calories",value:"301 Calories",show:true,single:true,sort:null},
+        {key:"Protein",value:"16g Protein",show:true,single:true,sort:null},
+        {key:"Fat",value:"13g Total Fat",show:true,single:true,sort:null},
+        {key:"Carbs",value:"34g Carbs",show:true,single:true,sort:null}
+      ]
+    },
+
+    {
+      name:"Clean Eating Shrimp And Asparagus Spaghetti Squash Spaghetti",
+      image:"https://webknox.com/recipeImages/835245-556x370.jpg",
+      link:"https://spoonacular.com/recipes/clean-eating-shrimp-and-asparagus-spaghetti-squash-spaghetti-835245",
+      kvtable:"<table><tr><td>Cost</td><td>$8.49 per serving</td></tr><tr><td>Calories</td><td>559 Calories</td></tr><tr><td>Protein</td><td>56g Protein</td></tr><tr><td>Fat</td><td>14g Total Fat</td></tr><tr><td>Carbs</td><td>59g Carbs</td></tr></table>",
+      dataPoints:[
+        {key:"Cost",value:"$8.49 per serving",show:true,single:true,sort:null},
+        {key:"Calories",value:"559 Calories",show:true,single:true,sort:null},
+        {key:"Protein",value:"56g Protein",show:true,single:true,sort:null},
+        {key:"Fat",value:"14g Total Fat",show:true,single:true,sort:null},
+        {key:"Carbs",value:"59g Carbs",show:true,single:true,sort:null}
+      ]
+    }
+  ]
+}
